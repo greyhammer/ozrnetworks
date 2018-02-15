124e2d98795599529b3467f5dbc327b693287a27
Keith Janak <kjjanak@gmail.com>
2018-02-15 16:27:51 -0600

    trying again

--- node/index.js
+++ node/index.js
@@ -3,8 +3,29 @@ const sh            = require('shelljs');
 const app           = express();
 const apiPort       = 8080;
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
 require('./routes')(app, sh);
 
-app.listen(apiPort, () => {
+http.listen(apiPort, () => {
     console.log('API lives on ' + apiPort);
 });
+
+// app.listen(apiPort, () => {
+//     console.log('API lives on ' + apiPort);
+// });
