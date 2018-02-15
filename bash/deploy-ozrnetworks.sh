8f278adc16c57558700b887815c7e0edd387af11
Keith Janak <kjjanak@gmail.com>
2018-02-15 13:24:45 -0600

    removed iis div labels, added print lines to bash script

--- bash/deploy-ozrnetworks.sh
+++ bash/deploy-ozrnetworks.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+echo 'changing dir to ozr web root'
 cd /var/www/ozrnetworks.com
+echo 'pulling latest code via git'
 git pull
+echo 'changing dir to ozr node dir'
 cd node
+echo 'installing required npm modules'
 npm install
+echo 'restarting node scripts'
 pm2 restart all
+echo 'done'
