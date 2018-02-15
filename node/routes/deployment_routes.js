a763527900b8aaf62b8f1f94c2674a8c8c28d312
Keith Janak <kjjanak@gmail.com>
2018-02-15 11:15:48 -0600

    using absolute path instead of relative

--- node/routes/deployment_routes.js
+++ node/routes/deployment_routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app, sh) {
     app.post('/deploy', (req, res) => {
         console.log('deploy post');
-        sh.exec("sh ../../bash/deploy-ozrnetworks.sh");
+        sh.exec("sh /var/www/ozrnetworks/bash/deploy-ozrnetworks.sh");
         res.send('deploy post');
     });
 };
