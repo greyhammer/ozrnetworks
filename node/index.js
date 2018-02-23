bc22f85fa343811cc6d05a97d82d2e543443d7c4
Keith Janak <kjjanak@gmail.com>
2018-02-22 22:29:11 -0600

    updating call to / to return online users

--- node/index.js
+++ node/index.js
@@ -3,6 +3,7 @@ const sh            = require('shelljs');
 const app           = express();
 const apiPort       = 8080;
 const fs            = require('fs');
+var usersOnline     = 0;
 
 var privateKey = fs.readFileSync('/home/keith/privkey.pem');
 var certificate = fs.readFileSync('/home/keith/fullchain.pem');
@@ -11,21 +12,20 @@ var credentials = {key: privateKey, cert: certificate};
 var https = require('https').Server(credentials, app);
 var io = require('socket.io')(https);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
   console.log('a user connected');
+  usersOnline ++;
   socket.on('disconnect', function(){
     console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    usersOnline--;
   });
 });
 
 require('./routes')(app, sh);
+
+app.get('/', function(req, res){
+  res.send(usersOnline);
+});
 
 https.listen(apiPort, () => {
     console.log('API lives on ' + apiPort);
