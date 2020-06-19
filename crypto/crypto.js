031268bd7b6509db90e526ea381e8cd70fe078d7
Keith Janak <kjanak@interviewstream.com>
2020-06-18 19:58:17 -0600

    testing jquery stuffs

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -6,5 +6,17 @@ $(document).ready(function() {
 $('#clear-txt-btn').click(function (){
     var clearTxt = $('#clear-txt').val();
     console.log(clearTxt);
+    console.log(encrypt(clearTxt));
 })
 
+function encrypt(message = '', key = ''){
+    var message = CryptoJS.AES.encrypt(message, key);
+    return message.toString();
+}
+
+function decrypt(message = '', key = ''){
+    var code = CryptoJS.AES.decrypt(message, key);
+    var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
+
+    return decryptedMessage;
+}
