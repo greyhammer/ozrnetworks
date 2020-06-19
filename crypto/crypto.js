785fd864c0d2dd6c424770d4db800ebcd3c8f47d
Keith Janak <kjanak@interviewstream.com>
2020-06-18 19:47:55 -0600

    adding form and started jsbits

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     console.log('crypto.js loaded');
 });
+
+
+$('#clear-txt-btn').click(function (){
+    var clearTxt = $('#clear-txt').val();
+    console.log(clearTxt);
+})
+
